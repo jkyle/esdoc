24d497dc2e435c52f45ae21e25583a62a2c8cc5e
J. Kyle Fagan <jkyle@causethought.com>
2016-09-13 16:02:42 -0500

    Sigh.

--- script/build.js
+++ script/build.js
@@ -3,8 +3,11 @@ var sh = require('./sh');
 
 sh.rm('./out/src');
 sh.mkdir('./out/src');
+console.log('Run 1.');
 sh.exec('./node_modules/.bin/babel --out-dir out/src src');
+console.log('Run 2.');
 sh.chmod('./out/src/ESDocCLI.js', '755');
+console.log('Run 3.');
 sh.cp('./src/Publisher/Builder/template/', './out/src/Publisher/Builder/template/');
 
 // build test
